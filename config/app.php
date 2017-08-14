f6f2182b1c21ae9fcc1ce1909d03e6c37c6aade0
Jason Lamb <jason@ridango.com>
2017-08-14 08:17:27 +0300

    Added Laravel Doctrine (ACL)

--- config/app.php
+++ config/app.php
@@ -185,6 +185,7 @@ return [
         LaravelDoctrine\Extensions\GedmoExtensionsServiceProvider::class,
         LaravelDoctrine\Extensions\BeberleiExtensionsServiceProvider::class,
         LaravelDoctrine\Migrations\MigrationsServiceProvider::class,
+        LaravelDoctrine\ACL\AclServiceProvider::class,
     ],
 
     /*
