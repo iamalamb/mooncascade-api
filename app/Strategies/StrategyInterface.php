ba69629e81018ad1d492b900bc60dd4fcc88c78c
Jason Lamb <jason@ridango.com>
2017-08-15 15:32:26 +0300

    Misc

---
+++ app/Strategies/StrategyInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Mooncascade\Strategies;
+
+/**
+ * Interface StrategyInterface
+ *
+ * Interface used to ensure correct implementation of the Strategy Design Pattern
+ * @see https://sourcemaking.com/design_patterns/strategy
+ */
+interface StrategyInterface
+{
+    /**
+     * Core function intended for concrete implementations
+     * to implement.
+     *
+     * @return mixed
+     */
+    public static function execute();
+}
