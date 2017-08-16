9c01bb8771caa80aac3cd26c4bf03d4f7a1a3cad
Jason Lamb <jason@ridango.com>
2017-08-16 08:41:30 +0300

    Additional tests for GenderRepository

--- tests/Feature/GenderRepositoryTest.php
+++ tests/Feature/GenderRepositoryTest.php
@@ -3,6 +3,10 @@
 namespace Tests\Feature;
 
 use LaravelDoctrine\ORM\Facades\EntityManager;
+use Mooncascade\Repositories\AbstractBaseRepository;
+use Doctrine\ORM\EntityRepository;
+use Doctrine\Common\Persistence\ObjectRepository;
+use Mooncascade\Repositories\GenderRepository;
 use Mooncascade\Entities\Gender;
 use Tests\TestCase;
 
@@ -20,6 +24,23 @@ class GenderRepositoryTest extends TestCase
         $this->repository = EntityManager::getRepository(Gender::class);
     }
 
+    public function testThatRepositoryIsCorrectType()
+    {
+        $this->assertInstanceOf(GenderRepository::class, $this->repository);
+        $this->assertInstanceOf(AbstractBaseRepository::class, $this->repository);
+        $this->assertInstanceOf(EntityRepository::class, $this->repository);
+        $this->assertInstanceOf(ObjectRepository::class, $this->repository);
+    }
+
+    /**
+     * Test to ensure that the getCount function returns the correct
+     * value based on the 'testing' environment settings.
+     */
+    public function testGetCountReturnsCorrectValue()
+    {
+        $count = $this->repository->getCount();
+        $this->assertEquals(2, $count);
+    }
 
     /**
      * Test to ensure that the findOneByRandom function correctly returns
