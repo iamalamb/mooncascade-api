6927f58f04f25a42cbe5a29d56be83343d19a25f
Jason Lamb <jason@ridango.com>
2017-08-16 08:38:55 +0300

    Additional tests for TeamRepository

--- tests/Feature/TeamRepositoryTest.php
+++ tests/Feature/TeamRepositoryTest.php
@@ -3,12 +3,20 @@
 namespace Tests\Feature;
 
 use LaravelDoctrine\ORM\Facades\EntityManager;
+use Mooncascade\Repositories\AbstractBaseRepository;
+use Doctrine\ORM\EntityRepository;
+use Doctrine\Common\Persistence\ObjectRepository;
+use Mooncascade\Repositories\TeamRepository;
 use Mooncascade\Entities\Team;
 use Tests\TestCase;
 
 class TeamRepositoryTest extends TestCase
 {
+    /**
+     * @var TeamRepository
+     */
     protected $repository;
+
 
     /**
      * Initial setup to ensure a fresh repository each time
@@ -20,6 +28,23 @@ class TeamRepositoryTest extends TestCase
         $this->repository = EntityManager::getRepository(Team::class);
     }
 
+    public function testThatRepositoryIsCorrectType()
+    {
+        $this->assertInstanceOf(TeamRepository::class, $this->repository);
+        $this->assertInstanceOf(AbstractBaseRepository::class, $this->repository);
+        $this->assertInstanceOf(EntityRepository::class, $this->repository);
+        $this->assertInstanceOf(ObjectRepository::class, $this->repository);
+    }
+
+    /**
+     * Test to ensure that the getCount function returns the correct
+     * value based on the 'testing' environment settings.
+     */
+    public function testGetCountReturnsCorrectValue()
+    {
+        $count = $this->repository->getCount();
+        $this->assertEquals(5, $count);
+    }
 
     /**
      * Test to ensure that the findOneByRandom function correctly returns
