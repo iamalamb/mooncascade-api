767492a00877047b7fda40762bd47e88348839d8
Jason Lamb <jason@ridango.com>
2017-09-08 08:48:26 +0300

    Misc refactoring

--- config/mooncascade.php
+++ config/mooncascade.php
@@ -8,8 +8,8 @@ return [
     'delay_race_start_time'                 => env('DELAY_RACE_START_TIME', 60),
     'delay_athlete_execution_min_threshold' => env('DELAY_ATHLETE_EXECUTION_MIN_THRESHOLD', 5),
     'delay_athlete_execution_max_threshold' => env('DELAY_ATHLETE_EXECUTION_MAX_THRESHOLD', 6),
-    'batch_athlete_retrieval_min_threshold' => env('BATCH_ATHELTE_RETRIEVAL_MIN_THRESHOLD', 1),
-    'batch_athlete_retrieval_max_threshold' => env('BATCH_ATHELTE_RETRIEVAL_MAX_THRESHOLD', 10),
+    'batch_athlete_retrieval_min_threshold' => env('BATCH_ATHELETE_RETRIEVAL_MIN_THRESHOLD', 1),
+    'batch_athlete_retrieval_max_threshold' => env('BATCH_ATHELETE_RETRIEVAL_MAX_THRESHOLD', 10),
     'available_strategies'                  => ['overtake', 'sequential', 'tie'],
     'gate_strategies'                       => ['sequential', 'tie'],
 ];
